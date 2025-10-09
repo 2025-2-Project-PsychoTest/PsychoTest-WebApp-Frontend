7f406cc1bf422cda7d4b1a1be44b18462378c6e4
VRojas1603 <rojas.vmanuel16@gmail.com>
2025-10-09 10:31:35 -0500

    feat: implement footer component.

---
+++ src/app/shared/presentation/components/footer-content/footer-content.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { TranslateModule } from "@ngx-translate/core";
+
+@Component({
+  selector: 'app-footer-content',
+  imports: [TranslateModule],
+  templateUrl: './footer-content.html',
+  styleUrl: './footer-content.css'
+})
+export class FooterContent {
+
+}
