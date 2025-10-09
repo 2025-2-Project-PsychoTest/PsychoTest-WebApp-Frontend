a3c9fd9b68f1390132103e016d3cd11b87b90ea3
VRojas1603 <rojas.vmanuel16@gmail.com>
2025-10-09 10:35:16 -0500

    feat: configure providers.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,26 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+import { provideAppInitializer, inject } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
+import { provideTranslateService, TranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    provideTranslateService({
+      loader: provideTranslateHttpLoader({prefix: './assets/i18n/', suffix: '.json'}),
+      defaultLanguage: 'en'
+    }),
+    provideAppInitializer(() => {
+      const translate = inject(TranslateService);
+      const browserLang = translate.getBrowserLang();
+      const langToUse = (browserLang === 'es' || browserLang === 'en') ? browserLang : 'en';
+      translate.use(langToUse);
+    })
   ]
 };
