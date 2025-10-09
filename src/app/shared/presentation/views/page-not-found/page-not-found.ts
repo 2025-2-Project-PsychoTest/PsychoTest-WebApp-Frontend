e2bd6c8bae8f9cf9784fb0c4c9b3ecb3e87a4a04
VRojas1603 <rojas.vmanuel16@gmail.com>
2025-10-09 10:32:34 -0500

    feat: implement page-not-found component.

---
+++ src/app/shared/presentation/views/page-not-found/page-not-found.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {Router } from '@angular/router';
+import {TranslatePipe} from '@ngx-translate/core';
+import {MatButton} from '@angular/material/button';
+
+@Component({
+  selector: 'app-page-not-found',
+  imports: [
+    TranslatePipe,
+    MatButton
+  ],
+  templateUrl: './page-not-found.html',
+  styleUrl: './page-not-found.css'
+})
+export class PageNotFound {
+  constructor(private router: Router) { }
+
+  goToPreviousPage() {
+    window.history.back();
+  }
+
+}
