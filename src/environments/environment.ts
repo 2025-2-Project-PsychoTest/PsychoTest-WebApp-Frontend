158d89511d44c66e0567f8552866be1604fb65bc
JoaquinC <u20201f788@upc.edu.pe>
2025-11-09 23:15:40 -0500

    fix: ddd structure

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+
+export const environment = {
+  production: true,
+  platformProviderApiBaseUrl: 'https://api.tu-dominio.com/api/v1',
+  platformProviderSubscriptionsEndpointPath: '/subscriptions'
+};
